150d9a80dbeb72e670d9d9d8360fd13493dc6b76
anguianoo <njanguiano@gmail.com>
2022-05-06 21:25:42 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const submitData = (name, email) => {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  };
+
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document.body.innerHTML = object["id"];
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      document.body.innerHTML = error.message;
+    });
+};
+
+submitData("dude", "dude@gmail.com");
